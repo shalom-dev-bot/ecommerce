c948ef9079ee6a3b52452d485bd2b2f3b61a41c5
shalomdev <shalomdev316@gmail.com>
2025-11-10 16:13:52 -0800

    first part

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shop(request, *args, **kwargs):
+    '''A view to render the shop homepage'''
+    context = {}
+    return render(request, 'shop/index.html', context)
