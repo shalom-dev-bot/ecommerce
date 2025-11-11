1d3f45ebd8de9186f00bc8ed37ff563a1bbef2df
shalomdev <shalomdev316@gmail.com>
2025-11-11 14:15:31 -0800

    template

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name='shop'),
+
+    path('panier/', views.panier, name = 'panier'),
+
+    path('commande/', views.commande, name = 'commande'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def shop(request, *args, **kwargs):
     '''A view to render the shop homepage'''
     context = {}
+
     return render(request, 'shop/index.html', context)
+
+
+def panier(request , *args, **kwargs):
+
+    context = {}
+
+    return render(request , 'shop/panier.html', context)
+
+
+def commande(request, *args , **kwargs):
+
+    context = {}
+
+    return render(request, 'shop/commande.html', context)
+
